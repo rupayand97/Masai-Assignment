aa3d215d0bdc6ccf4e5470f1fa30b6f147f30ce4
Rupayan Das <hrupayandas@gmail.com>
2024-12-28 20:25:57 +0530

    find the odd number in an array

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log(addNumbers(3,4,5));
 //program2: find even number in an array
 function findEvenNumber(arr){
     var result="";
-    for(let i=1; i<=arr.length;i++){
+    for(let i=0; i<=arr.length-1;i++){
         if(arr[i]%2==0){
             result += arr[i]+" "; 
         }
@@ -15,3 +15,15 @@ function findEvenNumber(arr){
     return result;
 }
 console.log(findEvenNumber([1,2,3,4,5,6]));
+
+// program3: find 0dd number in an array
+function findOddNumber(arr){
+    var res="";
+    for(let i=0; i<=arr.length-1;i++){
+        if(arr[i]%2 != 0){
+            res += arr[i]+" "; 
+        }
+    }
+    return res;
+}
+console.log(findOddNumber([1,2,3,4,5,6]));
