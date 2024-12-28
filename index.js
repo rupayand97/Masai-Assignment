b31b88f9c8998662c280c6299d974f3c27e09ab3
Rupayan Das <hrupayandas@gmail.com>
2024-12-28 20:20:37 +0530

    finding even numbers in an array

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function addNumbers(a,b,c){
     return a+b+c;
 }
 console.log(addNumbers(3,4,5));
+
+//program2: find even number in an array
+function findEvenNumber(arr){
+    var result="";
+    for(let i=1; i<=arr.length;i++){
+        if(arr[i]%2==0){
+            result += arr[i]+" "; 
+        }
+    }
+    return result;
+}
+console.log(findEvenNumber([1,2,3,4,5,6]));
